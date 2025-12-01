73a73895398593ed9c304bf92f9171310825d498
samuelmuyllaert-cpu <samuel.muyllaert@gmail.com>
2025-12-01 23:16:59 +0100

    Updated SEO.tsx

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -92,7 +92,6 @@ const SEO = ({
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
-      <link rel="manifest" href="/site.webmanifest" />
 
       {/* Additional Meta */}
       <meta name="format-detection" content="telephone=no" />
