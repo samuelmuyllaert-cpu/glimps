bc9c321fd24e8bc932e54c64e57fc0ae65328994
samuelmuyllaert-cpu <samuel.muyllaert@gmail.com>
2026-04-19 13:49:43 +0200

    Updated Lotana.tsx

--- src/pages/Lotana.tsx
+++ src/pages/Lotana.tsx
@@ -10,6 +10,15 @@ const Lotana = () => {
   const { language } = useLanguage();
 
   useEffect(() => {
+    const cleanupWidget = () => {
+      const existingScript = document.querySelector('#chatbotkit-widget');
+      if (existingScript) existingScript.remove();
+      document.querySelectorAll('[id^="chatbotkit"]').forEach(el => el.remove());
+      document.querySelectorAll('[class*="chatbotkit"]').forEach(el => el.remove());
+    };
+
+    cleanupWidget();
+
     const script = document.createElement('script');
     script.id = 'chatbotkit-widget';
     script.src = 'https://static.chatbotkit.com/integrations/widget/v2.js';
@@ -18,10 +27,7 @@ const Lotana = () => {
     document.body.appendChild(script);
 
     return () => {
-      const existingScript = document.querySelector('#chatbotkit-widget');
-      if (existingScript) {
-        existingScript.remove();
-      }
+      cleanupWidget();
     };
   }, []);
 
